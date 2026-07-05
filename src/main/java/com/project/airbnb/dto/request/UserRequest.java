c977361b066559a5be618cfd537c87ed1bbc2eaf
Shree <shreebiradar2005@gmail.com>
2026-07-05 14:57:21 +0530

    Add: Summary DTO user

--- src/main/java/com/project/airbnb/dto/request/UserRequest.java
+++ src/main/java/com/project/airbnb/dto/request/UserRequest.java
@@ -1,22 +1,12 @@
 package com.project.airbnb.dto.request;
 
-import com.project.airbnb.enums.Gender;
+import com.project.airbnb.dto.summary.UserSummary;
 import com.project.airbnb.enums.Role;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDate;
 
 public record UserRequest(
     @NotBlank String username,
-    @NotBlank String firstName,
-    @NotBlank String lastName,
-    @Email String email,
     @NotBlank @Size(min = 6) String password,
-    @NotBlank @Pattern(regexp = "^[0-9]{10}$", message = "Invalid mobile number") String mobile,
-    @NotNull Gender gender,
-    @Past LocalDate birthDate,
+    UserSummary summary,
     Role role) {}
