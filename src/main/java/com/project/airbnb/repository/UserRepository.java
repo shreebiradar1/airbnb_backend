414807368ce0379b22b2a9d395a8a863de770d33
Shree <shreebiradar2005@gmail.com>
2026-07-05 14:55:25 +0530

    Fix: Extends with JpaRepo

--- src/main/java/com/project/airbnb/repository/UserRepository.java
+++ src/main/java/com/project/airbnb/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.project.airbnb.repository;
 
+import com.project.airbnb.entities.UserEntity;
 import java.util.Optional;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.project.airbnb.entities.UserEntity;
-
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
   public Optional<UserEntity> findByUsername(String username);
 
   public Optional<UserEntity> findByEmail(String email);
