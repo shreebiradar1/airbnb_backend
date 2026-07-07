2fe7255e9e49fe03d26fa7fdd9a3c9f9ade6024b
Shree <shreebiradar2005@gmail.com>
2026-07-07 12:43:13 +0530

    Add: UserStatusRes dto

--- src/main/java/com/project/airbnb/dto/response/UserStatusRes.java
+++ src/main/java/com/project/airbnb/dto/response/UserStatusRes.java
@@ -2,4 +2,5 @@ package com.project.airbnb.dto.response;
 
 import com.project.airbnb.enums.UserStatus;
 
-public record UserStatusRes(String username, UserStatus oldStatus, UserStatus newStatus) {}
+public record UserStatusRes(
+    Long userId, String username, UserStatus oldStatus, UserStatus newStatus) {}
